e065bbfe1b2cb997eb9f73f9b3ab77b3fb24d415
Han <hanky_chung@hotmail.com>
2019-12-12 15:57:51 +0800

    add resolvePromise

--- promise.js
+++ promise.js
@@ -31,13 +31,6 @@ class MyPromise {
       setTimeout(() => {
         // 确保状态是未完成的情况下进行，否则有可能即处理onFulfilled又处理onRejected
         if (this.status !== PENDING) return
-        /**
-         * 若值是一个MyPromise实例，等待其执行原MyPromise实例传入的resolve/reject函数
-         * 即返回的value.then(resolve, reject)，实际上是原MyPromise将执行的完成嫁接于此MyPromise之上
-         */
-        if (value instanceof MyPromise) {
-          return value.then(resolve, reject)
-        }
         this.status = FULFILLED
         this.value = value
         this.resolves.forEach(fn => {
@@ -69,6 +62,7 @@ class MyPromise {
   }
 
   then(onFulfilled, onRejected) {
+    // *穿透*
     // 规范onFulfilled函数，默认为返回成功后的值
     onFulfilled =
       typeof onFulfilled === 'function' ? onFulfilled : value => value
@@ -79,30 +73,29 @@ class MyPromise {
         : error => {
             throw error
           }
-    if (this.status === FULFILLED) {
-      // 若此时promise状态为成功，立即执行onFulfilled，并且返回一个新的promise函数。
-      return new MyPromise(resolve => {
+
+    // then函数将返回一个新的promise实例
+    let promise2
+
+    promise2 = new MyPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
         let res = onFulfilled(this.value)
-        resolve(res)
+        resolvePromise(promise2, res, resolve, reject)
         // 此处不需要写reject的处理函数，executor执行过程出错会捕获错误给reject的处理函数
-      })
-    }
-    if (this.status === REJECTED) {
-      return new MyPromise(resolve => {
+      }
+      if (this.status === REJECTED) {
         let res = onRejected(this.reason)
-        resolve(res)
-      })
-    }
-    if (this.status === PENDING) {
-      /**
-       * 若此时promise状态为处理中，注册onFulfilled函数和onRejected函数
-       * 依然返回一个新的MyPromise，其状态改变的时刻取决于上一个MyPromise的状态改变时
-       */
-      return new MyPromise((resolve, reject) => {
+        resolvePromise(promise2, res, resolve, reject)
+      }
+      if (this.status === PENDING) {
+        /**
+         * 若此时promise状态为处理中，注册onFulfilled函数和onRejected函数
+         * 依然返回一个新的MyPromise，其状态改变的时刻取决于上一个MyPromise的状态改变时
+         */
         this.resolves.push(() => {
           try {
             let res = onFulfilled(this.value)
-            resolve(res)
+            resolvePromise(promise2, res, resolve, reject)
           } catch (e) {
             reject(e)
           }
@@ -110,14 +103,76 @@ class MyPromise {
         this.rejects.push(() => {
           try {
             let res = onRejected(this.reason)
-            resolve(res)
+            resolvePromise(promise2, res, resolve, reject)
           } catch (e) {
             reject(e)
           }
         })
-      })
-    }
+      }
+    })
+
+    return promise2
   }
 }
 
-module.exports = MyPromise
+function resolvePromise(promise2, res, resolve, reject) {
+  if (promise2 === res) throw 'error'
+  if (res instanceof MyPromise) {
+    res.then(
+      data => {
+        /**
+         * 若成功回调函数的参数是一个MyPromise实例，等待其执行原MyPromise实例传入的resolve/reject函数
+         * 即返回的res.then(resolve, reject)，实际上是原MyPromise将执行的完成嫁接于此MyPromise之上
+         * 此处需递归调用
+         */
+        resolvePromise(promise2, data, resolve, reject)
+      },
+      error => {
+        reject(error)
+      }
+    )
+  } else {
+    resolve(res)
+  }
+}
+
+// test
+new MyPromise(resolve => {
+  setTimeout(() => {
+    resolve('first')
+  }, 1000)
+})
+  .then(res => {
+    console.log(res)
+    return new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        reject(res + 'second')
+      }, 1000)
+    })
+  })
+  .then(
+    res => {
+      console.log(res)
+      return new MyPromise(resolve => {
+        setTimeout(() => {
+          resolve(res + 'third')
+        }, 1000)
+      })
+    },
+    err => {
+      console.log(err)
+      return new MyPromise((resolve, reject) => {
+        setTimeout(() => {
+          reject(err + 'err-third')
+        }, 1000)
+      })
+    }
+  )
+  .then(
+    res => {
+      console.log(res)
+    },
+    rej => {
+      console.log('err', rej)
+    }
+  )
